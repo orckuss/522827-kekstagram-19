a4e9eaeec7521fb5e762231bd59d3524f9c95750
Dmitry Vasilev <orckuss@gmail.com>
2020-03-18 03:39:11 +0300

    Implement post http request

--- js/form-dialog.js
+++ js/form-dialog.js
@@ -3,14 +3,24 @@
 (function () {
   var body = document.querySelector('body');
   var uploadFile = document.querySelector('#upload-file');
-  var editForm = document.querySelector('.img-upload__overlay');
-  var closeBtn = editForm.querySelector('#upload-cancel');
+  var editForm = document.querySelector('.img-upload__form');
+  var editFormView = editForm.querySelector('.img-upload__overlay');
+  var closeBtn = editFormView.querySelector('#upload-cancel');
 
   var hashTagField = document.querySelector('.text__hashtags');
   var commentsField = document.querySelector('.text__description');
 
   uploadFile.addEventListener('change', onFileUpload);
   closeBtn.addEventListener('click', onCloseButtonClick);
+
+  editForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    window.http.post(new FormData(editForm), onSuccess);
+  });
+
+  function onSuccess() {
+    closeEditForm();
+  }
 
   function onFileUpload() {
     showEditForm();
@@ -22,7 +32,7 @@
 
   function showEditForm() {
     body.classList.add('modal-open');
-    editForm.classList.remove('hidden');
+    editFormView.classList.remove('hidden');
     document.addEventListener('keydown', onEscPressed);
     window.formFilter.setDefault();
     window.scale.setDefault();
@@ -31,7 +41,7 @@
   function closeEditForm() {
     uploadFile.value = '';
     body.classList.remove('modal-open');
-    editForm.classList.add('hidden');
+    editFormView.classList.add('hidden');
     document.removeEventListener('keydown', onEscPressed);
   }
 
