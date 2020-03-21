98c589e22dcd8a21a4eb50dfb466fbac200a7312
Dmitry Vasilev <orckuss@gmail.com>
2020-03-21 15:27:36 +0300

    Add render module

--- js/gallery.js
+++ js/gallery.js
@@ -1,15 +1,10 @@
 'use strict';
 
 (function () {
-  var picturesContainer = document.querySelector('.pictures');
-  var fragment = document.createDocumentFragment();
-  var photoTemplate = document.querySelector('#picture')
-    .content.querySelector('.picture');
-
   window.http.get(onSuccess);
 
   function onSuccess(response) {
-    renderPhotos(response);
+    window.render(response);
 
     document.querySelectorAll('.picture')
       .forEach(function (picture, index) {
@@ -18,29 +13,6 @@
           window.pictureDialog.show(response[index]);
         });
       });
-  }
-
-  function renderPhotos(photos) {
-    photos.forEach(function (photo) {
-      fragment.append(createPhotoELementFromTemplate(photo));
-    });
-
-    picturesContainer.append(fragment);
-  }
-
-  function createPhotoELementFromTemplate(photo) {
-    var photoElement = photoTemplate.cloneNode(true);
-
-    photoElement.querySelector('.picture__img')
-      .src = photo.url;
-
-    photoElement.querySelector('.picture__likes')
-      .textContent = photo.likes;
-
-    photoElement.querySelector('.picture__comments')
-      .textContent = photo.comments.length;
-
-    return photoElement;
   }
 
 })();
